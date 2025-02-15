7fc64f08ff8717d96dd4c28139c65f2dfaf55318
guptaji <akhilshorts87@gmail.com>
2025-02-15 23:27:07 +0530

    phone navbar fixed

--- src/components/pages/Contest.jsx
+++ src/components/pages/Contest.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Contest = () => {
     return (
         <div>
-            <h1>THIs is contest page</h1>
+            <h1>This is contesh page !! under development </h1>
         </div>
     );
 }
